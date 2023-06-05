6453331bb4230a6186e6ea1552069efc22871bb1
Vitor Negromonte <vitorncabral@gmail.com>
2023-06-05 14:38:00 -0300

    Questões C e D da lista 0

---
+++ 1st_list/qC.py
@@ -0,0 +1,15 @@
+a = int(input()) # Arthur
+l = int(input()) # Luiz
+p = int(input()) # Pedro
+h = int(input()) # Duração da competição (em horas)
+
+# Calculando o valor máximo
+x = (a + l + abs(a - l))/2
+y = (x + p + abs(x - p))/2
+
+# Determinando o valor máximo obtido por um participante no intervalo da competição
+maximo = y * h
+
+# Imprime o valor máximo
+print(int(maximo))
+
