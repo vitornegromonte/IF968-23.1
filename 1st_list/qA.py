48b092c5461587288e8645b5e67a41b161f342bc
Vitor Negromonte <vitorncabral@gmail.com>
2023-06-05 21:27:12 -0300

    Questão A da lista 0

---
+++ 1st_list/qA.py
@@ -0,0 +1,24 @@
+# Arthur 10
+# Luiz 30
+# Pedro 100
+# Input D, o número de diamantes requeridos por Tantan
+# Output o nome da oferta aceita
+
+## Case 1:
+### Input: 20, Output: Luiz
+## Case 2:
+### Input: 120, Output: Nenhum
+## Case 3:
+### Input: 5, Output: Arthur
+
+# Input de diamantes requeridos por Tantan
+d = int(input())
+
+if d > 100:
+  print('Nenhum')
+elif d > 30 and d <= 100:
+  print('Pedro')
+elif d > 10 and d <= 30:
+  print('Luiz')
+else:
+  print('Arthur')
